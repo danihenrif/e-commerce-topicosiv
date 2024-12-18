a56ff4d1ce93bd33ef14ab00936e87f932d90cb2
danielhrqs4@yahoo.com <danielhrqs4@yahoo.com>
2024-12-17 22:05:58 -0300

    porcentagem erro em store product

--- store/src/main/java/br/ufrn/imd/store/controller/ProductController.java
+++ store/src/main/java/br/ufrn/imd/store/controller/ProductController.java
@@ -3,6 +3,8 @@ package br.ufrn.imd.store.controller;
 import br.ufrn.imd.store.model.Product;
 import br.ufrn.imd.store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +18,11 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public Product getProduct(@PathVariable int id){
+    public ResponseEntity<Product> getProduct(@PathVariable int id){
+        if (Math.random() < 0.2) {
+            return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+        }
         Product product = productService.getProductById(id);
-        return product;
+        return ResponseEntity.ok(product);
     }
 }
